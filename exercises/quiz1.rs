0c078f7ff77385d2e7dbe59c2ca1ba7b6e193ff4
Sarthak Kapila <112886451+sarthakkapila@users.noreply.github.com>
2024-06-26 12:19:26 +0000

    Quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,8 +16,13 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(apples: i32) -> i32{
+    if apples > 40 {
+        apples * 1
+    }else{
+        apples * 2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
